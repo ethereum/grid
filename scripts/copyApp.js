a9e0c5c87bb4f50d463b541a5c6c9bb5e96416b2
Everton Fraga <ev@ethereum.org>
2018-10-28 02:07:15 +0200

    Updating electron-app-updater

--- scripts/copyApp.js
+++ scripts/copyApp.js
@@ -2,3 +2,18 @@
 // TODO executed before electron builder builds: copies or downloads with app-updater a version of the latest .asar to this dir for shipping
 // TODO make sure there is not more than one asar present
 
+const path = require('path');
+const {AppUpdater} = require('@philipplgh/electron-app-updater');
+
+
+const updater = new AppUpdater({
+  repo: 'https://github.com/PhilippLgh/mist-ui-react'
+});
+
+(async function(){
+
+  const latest = await updater.remote.getLatest();
+
+  await updater.downloadUpdate(latest, path.join(__dirname, '..'));
+
+})();
