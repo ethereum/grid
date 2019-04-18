31a54cd0326e86d4c03063ee9aa3b4b3e1ee987e
PhilippLgh <philipp@ethereum.org>
2019-04-18 23:47:44 +0200

    fix: add ipc connection support to parity

--- ethereum_clients/client_plugins/parity.js
+++ ethereum_clients/client_plugins/parity.js
@@ -1,3 +1,25 @@
+const HOME = require('os').homedir()
+let BASE
+switch (process.platform) {
+  case 'win32': {
+    BASE = `${process.env.USERPROFILE}/AppData/Roaming/Parity/Ethereum`
+    break
+  }
+  case 'linux': {
+    BASE = '~/.local/share/io.parity.ethereum'
+    break
+  }
+  case 'darwin': {
+    // WARNING don't just use ~/Library/.. here
+    BASE = `${HOME}/Library/Application Support/io.parity.ethereum`
+    break
+  }
+  default: {
+  }
+}
+
+const IPC_PATH = `${BASE}/jsonrpc.ipc`
+
 module.exports = {
   type: 'client',
   order: 2,
@@ -6,5 +28,11 @@ module.exports = {
   // repository: 'https://github.com/paritytech/parity-ethereum'
   repository: 'https://github.com/PhilippLgh/EthCapetownWorkshop',
   prefix: `${process.platform}`, // filter github assets
-  binaryName: process.platform === 'win32' ? 'parity.exe' : 'parity'
+  binaryName: process.platform === 'win32' ? 'parity.exe' : 'parity',
+  resolveIpc: logs => IPC_PATH,
+  config: {
+    default: {
+      '--ipc-path': IPC_PATH
+    }
+  }
 }
---
+++ test/parity_test.js
@@ -0,0 +1,51 @@
+const { assert } = require('chai')
+const fs = require('fs')
+const path = require('path')
+const { PluginHost } = require('../ethereum_clients/PluginHost')
+
+describe('Clients', function() {
+  describe('Parity', function() {
+    it('establishes an IPC connection', async function() {
+      this.timeout(2 * 60 * 1000)
+      const pluginHost = new PluginHost()
+      const parity = pluginHost.getPluginByName('parity')
+      assert.isDefined(parity, 'parity plugin loaded and found')
+      const releases = await parity.getReleases()
+      let latest = releases[0]
+      if (latest.remote) {
+        latest = await parity.download(latest, progress => {
+          console.log('progress', progress)
+        })
+      }
+      // console.log('exists', latest.location, latest.remote)
+      assert.isTrue(
+        fs.existsSync(latest.location),
+        'local parity package exists'
+      )
+      // will find or extract the binary from package
+      const { binaryPath } = await parity.getLocalBinary()
+      assert.isTrue(
+        fs.existsSync(binaryPath),
+        'parity executable was extracted'
+      )
+
+      // both works: with or without specified IPC path
+      const config = {} // parity.config
+
+      return new Promise((resolve, reject) => {
+        parity.on('log', log => {
+          console.log('log', log)
+        })
+        parity.on('started', () => {
+          console.log('started...')
+        })
+        parity.on('connected', () => {
+          console.log('connected!')
+          parity.stop()
+          resolve()
+        })
+        parity.start(latest, config)
+      })
+    })
+  })
+})
