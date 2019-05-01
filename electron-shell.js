85a1fff014e8ddef33e9f5af4f49224d73a1aa1f
Everton Fraga <ev@ethereum.org>
2019-05-01 15:46:55 -0400

    fix: open links on default browser

--- electron-shell.js
+++ electron-shell.js
@@ -2,35 +2,38 @@ const path = require('path')
 const url = require('url')
 const Module = require('module')
 const fs = require('original-fs')
-const { app } = require('electron')
+const { app, shell } = require('electron')
 const WindowManager = require('./WindowManager')
 
 let win
 
 // downloads the app.zip from github to memory and starts it in a renderer
-const hotLoading = async (appUpdater) => {
-
-  let result = await appUpdater.hotLoad(path.join(__dirname, 'ui', 'download-splash.html'))
+const hotLoading = async appUpdater => {
+  let result = await appUpdater.hotLoad(
+    path.join(__dirname, 'ui', 'download-splash.html')
+  )
   // create window for app
   win = WindowManager.createWindow()
   win.loadURL(result.electronUrl)
 
   // win.webContents.openDevTools()
-
 }
 
 /*
-* the "packaged" app is the one that was shipped with this shell / which was part of the installation bundle
-* contrary: updated apps are downloaded and cached by the updater some time after installation and are not always available
-*/
+ * the "packaged" app is the one that was shipped with this shell / which was part of the installation bundle
+ * contrary: updated apps are downloaded and cached by the updater some time after installation and are not always available
+ */
 function loadPackagedApp() {
-
   let files = fs.readdirSync(__dirname)
-  let appPath = files.find(file => file.endsWith('.zip') || file.endsWith('.asar'))
+  let appPath = files.find(
+    file => file.endsWith('.zip') || file.endsWith('.asar')
+  )
   appPath = path.join(__dirname, appPath)
 
   if (!fs.existsSync(appPath)) {
-    throw new Error("Bad installation: application is missing or cannot be found")
+    throw new Error(
+      'Bad installation: application is missing or cannot be found'
+    )
     return
   }
 
@@ -72,27 +75,26 @@ function loadPackagedApp() {
   })
 }
 
-function secureApplication(){
+function secureApplication() {
   // FIXME replace with correct origin
   let validOrigin = 'https://yourapp.com/'
 
   app.on('web-contents-created', (event, contents) => {
-  
     // https://electronjs.org/docs/tutorial/security#11-verify-webview-options-before-creation
     contents.on('will-attach-webview', (event, webPreferences, params) => {
       // Strip away preload scripts if unused or verify their location is legitimate
       delete webPreferences.preload
       delete webPreferences.preloadURL
-  
+
       // Disable Node.js integration
       webPreferences.nodeIntegration = false
-  
+
       // Verify URL being loaded
       if (!params.src.startsWith(validOrigin)) {
         event.preventDefault()
       }
     })
-  
+
     // https://electronjs.org/docs/tutorial/security#12-disable-or-limit-navigation
     contents.on('will-navigate', (event, navigationUrl) => {
       const parsedUrl = new URL(navigationUrl)
@@ -100,16 +102,15 @@ function secureApplication(){
         event.preventDefault()
       }
     })
-  
+
     // https://electronjs.org/docs/tutorial/security#13-disable-or-limit-creation-of-new-windows
     contents.on('new-window', (event, navigationUrl) => {
       // In this example, we'll ask the operating system
       // to open this event's url in the default browser.
       event.preventDefault()
-  
+
       shell.openExternal(navigationUrl)
     })
-  
   })
 
   // Quit when all windows are closed.
@@ -122,9 +123,19 @@ function secureApplication(){
   })
 }
 
-function createRenderer(clientUrl){
-
+function createRenderer(clientUrl) {
   // secureApplication()
+
+  app.on('web-contents-created', (event, contents) => {
+    contents.on('will-navigate', (event, navigationUrl) => {
+      const parsedUrl = new URL(navigationUrl)
+      console.log('parsedUrl', parsedUrl)
+      if (parsedUrl.protocol === 'https:') {
+        shell.openExternal(navigationUrl)
+      }
+      event.preventDefault()
+    })
+  })
 
   const loadRenderer = () => {
     win = WindowManager.createInsecureWindow()
@@ -133,14 +144,13 @@ function createRenderer(clientUrl){
     // win.webContents.openDevTools()
   }
 
-  if(app.isReady()) {
+  if (app.isReady()) {
     loadRenderer()
-  }else{
+  } else {
     app.once('ready', loadRenderer)
   }
 
   return win
 }
-
 
 module.exports = createRenderer
