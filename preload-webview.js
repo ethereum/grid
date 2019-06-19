0f4c357ae31cb6f73037b047b8f14192977f3e84
Ryan Ghods <ralxzryan@gmail.com>
2019-06-19 05:35:58 -0700

    feat: os notifications (#252)
    * feat: os notifications
    
    * chore: utils/electron.js
    
    * chore: move electron utils file to utils/renderer and add to package.json

--- preload-webview.js
+++ preload-webview.js
@@ -1,4 +1,6 @@
 const { ipcRenderer, remote, webFrame } = require('electron')
+const { notify, openFolderDialog } = require('./utils/renderer/electron')
+
 const PluginHost = remote.getGlobal('PluginHost')
 
 const clientInterface = client => {
@@ -29,7 +31,25 @@ window.grid = {
     return PluginHost.getAllPlugins().map(client => clientInterface(client))
   },
   getClient: name => {
-    const client = PluginHost.getAllPlugins().find(p => p.name === name)
-    return clientInterface(client)
-  }
+    let client = PluginHost.getAllPlugins().find(p => p.name === name)
+    return {
+      sendRpc: async (method, params) => {
+        return client.rpc(method, params)
+      },
+      getState: () => {
+        return client.state
+      },
+      execute: command => {
+        client.execute(command)
+      },
+      on: (eventName, handler) => {
+        return client.on(eventName, handler)
+      },
+      off: (eventName, handler) => {
+        return client.removeListener(eventName, handler)
+      }
+    }
+  },
+  notify,
+  openFolderDialog
 }
