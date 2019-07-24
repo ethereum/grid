834c297bbfa654eb3ad81fa2aa73d7abe3e259da
Ev <ev@ethereum.org>
2019-07-24 10:55:57 -0400

    feat: implements instance lock (#365)
    * feat: implements instance lock
    
    * docs: explaining whats behind instanceLock

--- nano.js
+++ nano.js
@@ -37,89 +37,94 @@ const mb = menubar({
   showDockIcon: true
 })
 
-const app = mb.app
-// make sure every webview has nodeIntegration turned off and has only access to the API defined by
-// preload-webview.js
-app.on('web-contents-created', (event, contents) => {
-  // https://electronjs.org/docs/tutorial/security#11-verify-webview-options-before-creation
-  contents.on('will-attach-webview', (event, webPreferences, params) => {
-    // Strip away preload scripts if unused or verify their location is legitimate
-    delete webPreferences.preload
-    delete webPreferences.preloadURL
+const init = function(mb) {
+  const app = mb.app
+  // make sure every webview has nodeIntegration turned off and has only access to the API defined by
+  // preload-webview.js
+  app.on('web-contents-created', (event, contents) => {
+    // https://electronjs.org/docs/tutorial/security#11-verify-webview-options-before-creation
+    contents.on('will-attach-webview', (event, webPreferences, params) => {
+      // Strip away preload scripts if unused or verify their location is legitimate
+      delete webPreferences.preload
+      delete webPreferences.preloadURL
 
-    // console.log('will attach webview')
+      // console.log('will attach webview')
+      webPreferences.preload = path.join(__dirname, 'preload-webview')
 
-    webPreferences.preload = path.join(__dirname, 'preload-webview')
-
-    // Disable Node.js integration
-    webPreferences.nodeIntegration = false
+      // Disable Node.js integration
+      webPreferences.nodeIntegration = false
+    })
   })
-})
 
-mb.on('ready', () => {
-  const pluginHost = registerGlobalPluginHost()
-  const appManager = registerGlobalAppManager()
+  mb.on('ready', () => {
+    const pluginHost = registerGlobalPluginHost()
+    const appManager = registerGlobalAppManager()
 
-  /* for testing:
-  appManager.launch({
-    name: 'grid-ui',
-    args: {
-      scope: {
-        component: 'terminal',
-        client: 'geth'
+    // Unsure of linux distros behavior with menubar
+    // so for now we will always show on launch
+    // if (!startMinimized) {
+    //   mb.showWindow()
+    // }
+    mb.showWindow()
+
+    mb.window.on('blur', function() {
+      // it prevents window from hiding if keepWindowOpen is checked on tray's context menu
+      !keepWindowOpen && mb.hideWindow()
+    })
+  })
+
+  // right-click menu for tray
+  mb.on('after-create-window', function() {
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Keep window open',
+        type: 'checkbox',
+        checked: keepWindowOpen,
+        click: () => {
+          keepWindowOpen = !keepWindowOpen
+        }
+      },
+      { type: 'separator' },
+      {
+        label: 'Feedback',
+        click: () => {
+          shell.openExternal(
+            'https://docs.google.com/forms/d/e/1FAIpQLSeJ4BtbvDVSnIFCKG6TmJo_tbSZql-NBZHes_-M6SyTDTjP0Q/viewform'
+          )
+          mb.hideWindow()
+        }
+      },
+      { type: 'separator' },
+      {
+        label: 'Quit',
+        click: () => {
+          mb.app.quit()
+        }
       }
+    ])
+    mb.tray.on('right-click', () => {
+      mb.tray.popUpContextMenu(contextMenu)
+    })
+  })
+}
+
+/**
+ * requestSingleInstanceLock makes your application a Single Instance Application - instead of
+ * allowing multiple instances of your app to run, this will ensure that only a
+ * single instance of your app is running, and other instances signal this instance
+ * and exit.
+ * https://github.com/electron/electron/blob/f6a29707b64bc2f7364f89096d187246bfc53765/docs/api/app.md#apprequestsingleinstancelock
+ */
+const gotTheLock = mb.app.requestSingleInstanceLock()
+// If user tries to open another instance of Grid, the new one will quit
+if (!gotTheLock) {
+  mb.app.quit()
+} else {
+  // When another Grid instance is trying to run, we tell the original instance to show Nano window.
+  mb.app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (mb.window) {
+      mb.showWindow()
     }
   })
-  */
-
-  // Unsure of linux distros behavior with menubar
-  // so for now we will always show on launch
-  // if (!startMinimized) {
-  //   mb.showWindow()
-  // }
-  mb.showWindow()
-
-  /*
-  mb.window.webContents.openDevTools({
-    mode: 'detach'
-  })
-  */
-  mb.window.on('blur', function() {
-    // it prevents window from hiding if keepWindowOpen is checked on tray's context menu
-    !keepWindowOpen && mb.hideWindow()
-  })
-})
-
-// right-click menu for tray
-mb.on('after-create-window', function() {
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Keep window open',
-      type: 'checkbox',
-      checked: keepWindowOpen,
-      click: () => {
-        keepWindowOpen = !keepWindowOpen
-      }
-    },
-    { type: 'separator' },
-    {
-      label: 'Feedback',
-      click: () => {
-        shell.openExternal(
-          'https://docs.google.com/forms/d/e/1FAIpQLSeJ4BtbvDVSnIFCKG6TmJo_tbSZql-NBZHes_-M6SyTDTjP0Q/viewform'
-        )
-        mb.hideWindow()
-      }
-    },
-    { type: 'separator' },
-    {
-      label: 'Quit',
-      click: () => {
-        mb.app.quit()
-      }
-    }
-  ])
-  mb.tray.on('right-click', () => {
-    mb.tray.popUpContextMenu(contextMenu)
-  })
-})
+  init(mb)
+}
